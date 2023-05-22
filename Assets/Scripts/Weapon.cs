e5327c7beba099667597384f3ff5a6aabe0da79f
sakura-kiki <sbilge7631@gmail.com>
2023-05-23 01:45:31 +0300

    Hit VFX and Enemy Damage

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -7,6 +7,8 @@ public class Weapon : MonoBehaviour
 {
     [SerializeField] Camera FPCamera;
     [SerializeField] float range = 100f;
+    [SerializeField] float damage = 30f;  //* The weapon's damage 
+    [SerializeField] ParticleSystem muzzleFlash;
 
     void Update()
     {
@@ -18,13 +20,35 @@ public class Weapon : MonoBehaviour
 
     private void Shoot()
     {
+        PlayMuzzleFlash();
+        ProcessRaycast();
+    }
+
+    void PlayMuzzleFlash()
+    {
+        muzzleFlash.Play();
+    }
+
+    void ProcessRaycast()
+    {
         //* In order to execute Raycast method we have to give it some parameters
         //* FPCamera.transform.position is for the starting point
         //* FPCamera.transform.forward is for the direction of the cast
         RaycastHit hit;
         //* This one will store the information about the object that the cast collided with.
-        Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range); //? Why we indicate out hit, what does it mean?
-
-        Debug.Log("I hit this thing:" + hit.transform.name);
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
+        {
+            Debug.Log("I hit this thing:" + hit.transform.name);
+            //TODO: Add some visual or sound hit effect for players
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            //* Prevent the error messages when we shoot the game props
+            if (target == null) return;
+            target.TakeDamage(damage);
+        }
+        //* This will prevent getting error messages when we shoot the sky/raycast goes to infinity
+        else
+        {
+            return;
+        }
     }
 }
